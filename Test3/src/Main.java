70d2c0d34435bc9d93956feb7530ad878cb2e777
sumblink <113610847+sumblink@users.noreply.github.com>
2022-09-18 11:34:17 -0400

    Add files via upload

---
+++ Test3/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        int arr[] = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+        int sum = 0;
+        for (int i = 1; i < arr.length; i++) {if(i%3==0) {
+            System.out.println(i);
+        }}}}
+
